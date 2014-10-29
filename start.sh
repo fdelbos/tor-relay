f59a33c6a3f84b3ac76721f26a3ae49e76d4e87d
Frederic Delbos <fred.delbos@gmail.com>
2014-10-29 17:53:20 +0100

    prefix must be 4 chars long

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 docker rm -f tor-relay
 
-docker build -t tor/tor-relay .
+docker build -t ator/tor-relay .
 
 docker run \
 	-d --restart=always \
 	--name tor-relay \
 	-v "$PWD/data/db:/data/db" \
 	-p 9001:9001 \
-	tor/tor-relay
+	ator/tor-relay
